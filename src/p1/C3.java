ac1fbe210eb0d925c9b10c717dabf37cd92a5d3c
7Alberto7 <j.albertocg86@gmail.com>
2013-10-21 14:00:47 +0200

    CD5

--- src/p1/C3.java
+++ src/p1/C3.java
@@ -4,6 +4,22 @@ public class C3 {
 	private String atr1;
 	private int atr2;
 
+	public String getAtr1() {
+		return atr1;
+	}
+
+	public void setAtr1(String atr1) {
+		this.atr1 = atr1;
+	}
+
+	public int getAtr2() {
+		return atr2;
+	}
+
+	public void setAtr2(int atr2) {
+		this.atr2 = atr2;
+	}
+
 	public C3(String atr1, int atr2) {
 		this.atr1 = atr1;
 		this.atr2 = atr2;
