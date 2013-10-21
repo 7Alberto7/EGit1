986b7333c85a772b6e06b1ec4d7304a41e6173a8
7Alberto7 <j.albertocg86@gmail.com>
2013-10-21 14:08:25 +0200

    CR1.04

--- src/p1/C2.java
+++ src/p1/C2.java
@@ -2,9 +2,11 @@ package p1;
 
 public class C2 {
 	private String atr1;
+	private String atr2;
 
-	public C2(String atr1) {
+	public C2(String atr1, String atr2) {
 		this.atr1 = atr1;
+		this.atr2 = atr2;
 	}
 
 	public String getAtr1() {
@@ -13,5 +15,13 @@ public class C2 {
 
 	public void setAtr1(String atr1) {
 		this.atr1 = atr1;
+	}
+
+	public String getAtr2() {
+		return atr2;
+	}
+
+	public void setAtr2(String atr2) {
+		this.atr2 = atr2;
 	}
 }
