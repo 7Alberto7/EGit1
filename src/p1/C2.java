30fab087063d4f6d726efbb92f395f38a5b8a3fc
7Alberto7 <j.albertocg86@gmail.com>
2013-10-21 14:02:50 +0200

    CR1.03

--- src/p1/C2.java
+++ src/p1/C2.java
@@ -6,4 +6,12 @@ public class C2 {
 	public C2(String atr1) {
 		this.atr1 = atr1;
 	}
+
+	public String getAtr1() {
+		return atr1;
+	}
+
+	public void setAtr1(String atr1) {
+		this.atr1 = atr1;
+	}
 }
