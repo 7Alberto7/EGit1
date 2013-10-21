44574d3108e2ce61496378c6c3f107a711bb5da0
7Alberto7 <j.albertocg86@gmail.com>
2013-10-21 14:06:50 +0200

    CD7

--- src/p1/C3.java
+++ src/p1/C3.java
@@ -4,6 +4,11 @@ public class C3 {
 	private String atr1;
 	private int atr2;
 
+	public C3(String atr1, int atr2) {
+		this.atr1 = atr1;
+		this.atr2 = atr2;
+	}
+
 	public String getAtr1() {
 		return atr1;
 	}
@@ -19,9 +24,4 @@ public class C3 {
 	public void setAtr2(int atr2) {
 		this.atr2 = atr2;
 	}
-
-	public C3(String atr1, int atr2) {
-		this.atr1 = atr1;
-		this.atr2 = atr2;
-	};
 }
